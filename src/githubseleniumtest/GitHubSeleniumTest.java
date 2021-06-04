15fd0a60888e1a028e2f99b920da298ee5fe7ce0
Veckler <veckler08@gmail.com>
2021-06-04 22:41:41 +0200

    read credentials

--- src/githubseleniumtest/GitHubSeleniumTest.java
+++ src/githubseleniumtest/GitHubSeleniumTest.java
@@ -1,23 +1,42 @@
 package githubseleniumtest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GitHubSeleniumTest {
-    public static void main(String[] args) {
-       System.setProperty("webdriver.gecko.driver","geckodriver.exe");
-       WebDriver driver = new FirefoxDriver();
-       
-        String baseUrl = "http://www.facebook.com";
-        String tagName = "";
 
-        driver.get(baseUrl);
-        
-        tagName = driver.findElement(By.id("email")).getTagName();
-        System.out.println(tagName);
-       
-        driver.close();
+    public static void main(String[] args) {
+
+        String username = "";
+        String password = "";
+
+        try {
+            File credentialsFile = new File("credentials.txt");
+            Scanner myReader = new Scanner(credentialsFile);
+
+            username = myReader.nextLine();
+            password = myReader.nextLine();
+
+            myReader.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GitHubSeleniumTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://github.com/Veckler/SeleniumTestDocs/blob/main/testcase.md");
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("login_field")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.name("commit")).click();
+//        driver.quit();
         System.exit(0);
     }
-    
 }
